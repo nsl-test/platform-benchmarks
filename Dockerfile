0a7f077ac6dabcc6d743d7a4f8e0ceb9fa8d02ea
rohit_verma <987@Open>
2022-05-19 10:17:35 +0530

    add nerdctl support

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,7 @@ COPY --from=redboxoss/scuttle:latest scuttle /bin/scuttle
 RUN apk add --no-cache ca-certificates && adduser -D -u 12345 -g 12345 k6
 COPY --from=builder /tmp/k6 /usr/bin/k6
 COPY --from=redboxoss/scuttle:latest scuttle /bin/scuttle
-COPY iam/libs /home/k6/
+COPY iam/libs /home/k6/libs
 
 USER 12345
 WORKDIR /home/k6
--- docker-run.sh
+++ docker-run.sh
@@ -20,4 +20,14 @@ shift
 SCRIPT_NAME=$MODULE_NAME/k6-script.js
 TAG_NAME="$(basename -s .js $SCRIPT_NAME)-$(date +%s)"
 
-docker compose run -T --rm  k6 run -<$SCRIPT_NAME --tag testid=$TAG_NAME "$@"
+if command -v nerdctl &> /dev/null
+then
+    nerdctl run --rm --network=platform-benchmarks_k6 -e K6_PROMETHEUS_REMOTE_URL=http://prometheus:9090/api/v1/write \
+    -e K6_OUT=output-prometheus-remote -v $(pwd)/$SCRIPT_NAME:/home/k6/k6-script.js ghcr.io/nsl-test/xk6:v0.6 \
+    run /home/k6/k6-script.js --tag testid=$TAG_NAME "$@"
+    exit
+else
+    docker compose run -T --rm  k6 run -<$SCRIPT_NAME --tag testid=$TAG_NAME "$@"
+fi
+
+
